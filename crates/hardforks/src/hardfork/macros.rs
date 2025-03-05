42a3427e19ec7a7dccfab308b21252ab62c2c5ae
Miguel Oliveira <migueloliveiradev@gmail.com>
2025-03-05 17:26:03 -0300

    feat: extract ethereum-forks as standalone crate (#1)
    * feat: extract ethereum-forks as standalone crate
    
    * chore: remove unnecessary comments in `Cargo.toml
    
    * chore: add license
    
    * chore: add SECURITY.md
    
    * chore: add clippy and rustfmt
    
    * ci: add workflow to the project
    
    * chore: add rusfmt configs
    
    * wip
    
    * deny.toml
    
    * move tests
    
    * fix macro
    
    * clippy
    
    * fix
    
    * rm active_at_head
    
    * fix test
    
    ---------
    
    Co-authored-by: Arsenii Kulikov <klkvrr@gmail.com>

---
+++ crates/hardforks/src/hardfork/macros.rs
@@ -0,0 +1,46 @@
+/// Macro that defines different variants of a chain specific enum. See [`crate::Hardfork`] as an
+/// example.
+#[macro_export]
+macro_rules! hardfork {
+    ($(#[$enum_meta:meta])* $enum:ident { $( $(#[$meta:meta])* $variant:ident ),* $(,)? }) => {
+        $(#[$enum_meta])*
+        #[derive(Debug, Copy, Clone, Eq, PartialEq, PartialOrd, Ord, Hash)]
+        pub enum $enum {
+            $( $(#[$meta])* $variant ),*
+        }
+
+        impl $enum {
+            /// Returns variant as `str`.
+            pub const fn name(&self) -> &'static str {
+                match self {
+                    $( $enum::$variant => stringify!($variant), )*
+                }
+            }
+        }
+
+        impl core::str::FromStr for $enum {
+            type Err = $crate::__private::String;
+
+            fn from_str(s: &str) -> Result<Self, Self::Err> {
+                match s.to_lowercase().as_str() {
+                    $(
+                        s if s == stringify!($variant).to_lowercase() => Ok($enum::$variant),
+                    )*
+                    _ => return Err($crate::__private::format!("Unknown hardfork: {s}")),
+                }
+            }
+        }
+
+        impl $crate::Hardfork for $enum {
+            fn name(&self) -> &'static str {
+                self.name()
+            }
+        }
+
+        impl core::fmt::Display for $enum {
+            fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+                write!(f, "{self:?}")
+            }
+        }
+    }
+}
