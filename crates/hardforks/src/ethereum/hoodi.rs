c98f9aa099f870bd4a149e3da5b95f6cc8b1fb56
Ishika Choudhury <117741714+Rimeeeeee@users.noreply.github.com>
2025-05-03 13:01:39 +0530

    refactor: added const without seperator for eth testnet (#34)
    refactor: added const without seperator for eth mainnet

--- crates/hardforks/src/ethereum/hoodi.rs
+++ crates/hardforks/src/ethereum/hoodi.rs
@@ -1,4 +1,4 @@
 //! Hoodi hardfork starting points
 
-/// Prague hoodi hard fork activation timestamp
+/// Prague hoodi hard fork activation timestamp is 1742999832.
 pub const HOODI_PRAGUE_TIMESTAMP: u64 = 1_742_999_832;
